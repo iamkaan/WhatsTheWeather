2aa8231079240f56be83d7ec2a2c44da1a2a81d2
iamkaan <kaanmamikoglu@gmail.com>
2015-11-25 17:59:46 +0000

    added Google Maps

--- app/src/main/java/com/iamkaan/whatstheweather/MainActivity.java
+++ app/src/main/java/com/iamkaan/whatstheweather/MainActivity.java
@@ -1,13 +1,26 @@
 package com.iamkaan.whatstheweather;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
